f09942783cfa64e3d26efdf8a9b79cc973abebed
shortsn <shortsn@users.noreply.github.com>
2023-05-07 20:31:09 +0000

    test

--- mqtt-inspect/addon/run.sh
+++ mqtt-inspect/addon/run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bashio
 
-export MQTT_HOST=$(bashio::services mqtt "host")
-export MQTT_USER=$(bashio::services mqtt "username")
-export MQTT_PASSWORD=$(bashio::services mqtt "password")
+bashio::log.info "Starting application..."
+
+if bashio::services.available "mqtt"; then
+    export MQTT_HOST=$(bashio::services "mqtt" "host")
+    export MQTT_USER=$(bashio::services "mqtt" "username")
+    export MQTT_PASSWORD=$(bashio::services "mqtt" "password")
+else
+    bashio::log.info "The mqtt addon is not available."
+fi
 
 exec node index.js
