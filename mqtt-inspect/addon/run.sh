1e23d822b994dcf326d97d903f5a4e9bf1d8a88d
shortsn <shortsn@users.noreply.github.com>
2023-05-07 19:43:01 +0000

    test

--- mqtt-inspect/addon/run.sh
+++ mqtt-inspect/addon/run.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bashio
 
-if ! bashio::services.available "mqtt"; then
-    bashio::log.error "No internal MQTT service found"
-else
-    bashio::log.info "MQTT service found, fetching credentials ..."
-    export MQTT_HOST=$(bashio::services mqtt "host")
-    export MQTT_USER=$(bashio::services mqtt "username")
-    export MQTT_PASSWORD=$(bashio::services mqtt "password")
-fi
+export MQTT_HOST=$(bashio::config "mqtt.server")
+export MQTT_USER=$(bashio::config 'mqtt.user')
+export MQTT_PASSWORD=$(bashio::config 'mqtt.password')
 
 exec node index.js
