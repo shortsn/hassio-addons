b92e98d1fe8e1a2e70b451f3eefedc1432ac095f
shortsn <shortsn@users.noreply.github.com>
2023-05-07 21:01:38 +0000

    test

--- mqtt-inspect/src/index.ts
+++ mqtt-inspect/src/index.ts
@@ -1,25 +1,30 @@
+import { connect } from 'async-mqtt';
 
-const { MQTT_HOST, MQTT_USER, MQTT_PASSWORD } = process.env
+const { MQTT_HOST, MQTT_PORT, MQTT_USER, MQTT_PASSWORD } = process.env
 
 console.log('starting')
-console.log(MQTT_HOST, MQTT_USER, MQTT_PASSWORD)
+console.log(MQTT_HOST, MQTT_PORT, MQTT_USER, MQTT_PASSWORD)
 
-// const client = connect("tcp://somehost.com:1883");
+const client = connect(`mqtt://${MQTT_HOST}`, {
+    username: MQTT_USER,
+    password: MQTT_PASSWORD,
+    port: typeof MQTT_PORT === 'string' ? parseInt(MQTT_PORT, 10) : undefined
+});
 
-// const doStuff = async () => {
+const doStuff = async () => {
+    console.log('Connected')
+    try {
+        const grants = await client.subscribe('#')
+        console.log('subscribed')
+    } catch (e) {
+        // Do something about it!
+        console.log(e.stack);
+        process.exit();
+    }
+}
 
-//     console.log("Starting");
-//     try {
-//         await client.publish("wow/so/cool", "It works!");
-//         // This line doesn't run until the server responds to the publish
-//         await client.end();
-//         // This line doesn't run until the client has disconnected without error
-//         console.log("Done");
-//     } catch (e) {
-//         // Do something about it!
-//         console.log(e.stack);
-//         process.exit();
-//     }
-// }
+client.on("connect", doStuff);
 
-// client.on("connect", doStuff);
+client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, payload.toString())
+})
