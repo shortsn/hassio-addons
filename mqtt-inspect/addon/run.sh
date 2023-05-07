7106e5c3344b5eb1e7ef9f9ed757c5ba04fdf822
shortsn <shortsn@users.noreply.github.com>
2023-05-07 20:34:45 +0000

    test

--- mqtt-inspect/addon/run.sh
+++ mqtt-inspect/addon/run.sh
@@ -1,13 +1,25 @@
-#!/usr/bin/env bashio
+#!/usr/bin/with-contenv bashio
 
 bashio::log.info "Starting application..."
 
-if bashio::services.available "mqtt"; then
-    export MQTT_HOST=$(bashio::services "mqtt" "host")
-    export MQTT_USER=$(bashio::services "mqtt" "username")
-    export MQTT_PASSWORD=$(bashio::services "mqtt" "password")
+if bashio::config.has_value 'mqtt_host' \
+    && bashio::config.has_value 'mqtt_port' \
+    && bashio::config.has_value 'mqtt_user' \
+    && bashio::config.has_value 'mqtt_password';
+then
+    export MQTT_HOST=$(bashio::config 'mqtt_host')
+    export MQTT_PORT=$(bashio::config 'mqtt_port')
+    export MQTT_USER=$(bashio::config 'mqtt_user')
+    export MQTT_PASSWORD=$(bashio::config 'mqtt_password')
+    bashio::log.info "Using configured MQTT Host $MQTT_HOST:$MQTT_PORT"
+elif ! bashio::services.available "mqtt"; then
+    bashio::exit.nok "No MQTT broker configured and no internal MQTT service found"
 else
-    bashio::log.info "The mqtt addon is not available."
+    export MQTT_HOST=$(bashio::services mqtt "host")
+    export MQTT_PORT=$(bashio::services mqtt "port")
+    export MQTT_USER=$(bashio::services mqtt "username")
+    export MQTT_PASSWORD=$(bashio::services mqtt "password")
+    bashio::log.info "Using internal MQTT Host $MQTT_HOST:$MQTT_PORT"
 fi
 
 exec node index.js
